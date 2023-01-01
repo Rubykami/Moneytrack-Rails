d28230ecbfeed567a39f9ef6b23e39c7f4293418
Bruno Leandro <bruno.lbs100@gmail.com>
2023-01-01 01:04:41 -0300

    Config devise_token_auth migration to correct parameters

--- db/migrate/20230101040011_devise_token_auth_create_users.rb
+++ db/migrate/20230101040011_devise_token_auth_create_users.rb
@@ -23,19 +23,18 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
 
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+     ## Lockable
+      t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
 
       ## User Info
       t.string :name
-      t.string :nickname
-      t.string :image
-      t.string :email
+      t.string :email, null: false, default: ''
+      t.boolean :admin
 
       ## Tokens
-      t.json :tokens
+      t.text :tokens
 
       t.timestamps
     end
@@ -44,6 +43,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :unlock_token,         unique: true
   end
 end
