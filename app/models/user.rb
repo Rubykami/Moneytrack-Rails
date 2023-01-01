741c7c49cfed5ddff81571b19d6b17275a7ea9ec
Bruno Leandro <bruno.lbs100@gmail.com>
2023-01-01 01:06:46 -0300

    Config User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :lockable, :confirmable, :timeoutable
   include DeviseTokenAuth::Concerns::User
+  has_many :accounts
+
+  before_create {
+    self.id = SecureRandom.uuid
+  }
+
+  
+
 end
