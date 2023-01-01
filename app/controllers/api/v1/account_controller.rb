418808c32fe744d89eead93d03a8dd3a16104be5
Bruno Leandro <bruno.lbs100@gmail.com>
2023-01-01 01:21:40 -0300

    Config Account Controller

---
+++ app/controllers/api/v1/account_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::AccountController < ApplicationController
+
+    def index 
+        @accounts = Account.all 
+        render json: @accounts
+    end 
+
+    def create 
+        @account = Account.new(account_params)
+        if @account.save 
+            render json: @account, status: :created
+        else
+            render json: @account.errors, status: :unprocessable_entity
+        end
+    end 
+
+    def destroy 
+        @account = Account.find(params[:id])
+        @account.destroy
+
+    end
+
+    def show 
+        @account = Account.find(params[:id])
+        render json: @account
+    end 
+
+    def new 
+        
+        
+    end
+
+    private 
+
+    def account_params 
+        params.require(:account).permit(:name, :ownername, :ownercpfnumber, :accountnumber, :securitycode, :balancevalue, :accounttype, :user_id)
+    end 
+end
