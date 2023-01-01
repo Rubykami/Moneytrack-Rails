00bc7be0c41d2a53739eec2bc1a666d610779a2c
Bruno Leandro <bruno.lbs100@gmail.com>
2023-01-01 01:55:05 -0300

    Config to use UUID as ID

--- db/migrate/20230101044217_create_accounts.rb
+++ db/migrate/20230101044217_create_accounts.rb
@@ -1,7 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration[7.0]
   def change
-    create_table :accounts do |t|
+    create_table :accounts, id: :uuid do |t|
       t.string :name
+      t.uuid :user_id
       t.string :ownername
       t.string :ownercpfnumber
       t.string :accountnumber
@@ -11,5 +12,7 @@ class CreateAccounts < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+
+    add_index :accounts, :user_id
   end
 end
