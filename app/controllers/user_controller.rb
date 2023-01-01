7d26f666a3fbf258d0c5cb1fa2f2ea8f9e197a50
Bruno Leandro <bruno.lbs100@gmail.com>
2023-01-01 01:20:27 -0300

    Config User Controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::UserController < ApplicationController
+    before_action :authenticate_api_user!
+    wrap_parameters :user, include: [:password, :password_confirmation]
+
+    def index 
+        @user = User.all 
+        render json: @user
+    end 
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            render json: @user, status: :created 
+        else
+            render json: @user.errors_full_message, status: :unprocessable_entity
+        end
+    end 
+
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+
+    end
+
+    def show 
+        @user = User.find(params[:id])
+        render json: @user
+    end 
+
+    def new 
+        
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end 
+end
